f80337cfa53bd0d15b6620283efac1e60dbe2868
fonarevvichka <Vichkata222@gmail.com>
2016-05-18 19:50:12 -0400

    picture uploaded, nuked login code, pages still exist but have been torn down to scratch, will start over soon

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-function checkPassword () {
-  var password = "4029";
-  var userInput = document.getElementById("pswd").value;
-  if (password.localeCompare(userInput) == 0) {
-    // password was correct
-    alert("password correct");
-    }
-  else {
-    // Incorrect password.
-    alert("password incorrect");
-  }
+// function checkPassword () {
+//   var password = "4029";
+//   var userInput = document.getElementById("pswd").value;
+//   if (password.localeCompare(userInput) == 0) {
+//     // password was correct
+//     window.open("markusthemuskrat/adminPage")
+//     }
+//   else {
+//     // Incorrect password.
+//     alert("password incorrect");
+//   }
+// }
+
+<SCRIPT LANGUAGE="javascript">
+
+var getin = prompt("What is the password?","")
+if (getin=="peppermint")
+{
+alert('You got it! In you go...')
+location.href='scripttip73correct.html'
 }
+else
+{
+if (getin=="null")
+{location.href='nope2.,html'}
+else
+if (getin!="peppermint")
+{location.href='nope.html'}
+}
+</SCRIPT>
